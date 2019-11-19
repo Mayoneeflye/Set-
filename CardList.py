263096e1214f5700f991abed0e577d42b26a1012
Mayoneeflye <43866338+Mayoneeflye@users.noreply.github.com>
2019-11-18 23:19:24 -0500

    Add files via upload

---
+++ CardList.py
@@ -0,0 +1,103 @@
+OneBlankBlueDiamond  = "Cards/OneBlankBlueDiamond.png", "One", "Blank", "Blue", "Diamond"
+OneBlankBlueRound = "Cards/OneBlankBlueRound.png", "One", "Blank", "Blue", "Round"
+OneBlankBlueSquiggle = "Cards/OneBlankBlueSquiggle.png", "One", "Blank", "Blue", "Squiggle"
+OneBlankGreenDiamond = "Cards/OneBlankGreenDiamond.png", "One", "Blank", "Green", "Diamond"
+OneBlankGreenRound = "Cards/OneBlankGreenRound.png", "One", "Blank", "Green", "Round"
+OneBlankGreenSquiggle = "Cards/OneBlankGreenSquiggle.png", "One", "Blank", "Green", "Squiggle"
+OneBlankRedDiamond = "Cards/OneBlankRedDiamond.png", "One", "Blank", "Red", "Diamond"
+OneBlankRedRound = "Cards/OneBlankRedRound.png", "One", "Blank", "Red", "Round"
+OneBlankRedSquiggle = "Cards/OneBlankRedSquiggle.png", "One", "Blank", "Red", "Squiggle"
+OneFilledBlueDiamond  = "Cards/OneFilledBlueDiamond.png", "One", "Filled", "Blue", "Diamond"
+OneFilledBlueRound = "Cards/OneFilledBlueRound.png", "One", "Filled", "Blue", "Round"
+OneFilledBlueSquiggle = "Cards/OneFilledBlueSquiggle.png", "One", "Filled", "Blue", "Squiggle"
+OneFilledGreenDiamond = "Cards/OneFilledGreenDiamond.png", "One", "Filled", "Green", "Diamond"
+OneFilledGreenRound = "Cards/OneFilledGreenRound.png", "One", "Filled", "Green", "Round"
+OneFilledGreenSquiggle = "Cards/OneFilledGreenSquiggle.png", "One", "Filled", "Green", "Squiggle"
+OneFilledRedDiamond = "Cards/OneFilledRedDiamond.png", "One", "Filled", "Red", "Diamond"
+OneFilledRedRound = "Cards/OneFilledRedRound.png", "One", "Filled", "Red", "Round"
+OneFilledRedSquiggle = "Cards/OneFilledRedSquiggle.png", "One", "Filled", "Red", "Squiggle"
+OneStripedBlueDiamond  = "Cards/OneStripedBlueDiamond.png", "One", "Striped", "Blue", "Diamond"
+OneStripedBlueRound = "Cards/OneStripedBlueRound.png", "One", "Striped", "Blue", "Round"
+OneStripedBlueSquiggle = "Cards/OneStripedBlueSquiggle.png", "One", "Striped", "Blue", "Squiggle"
+OneStripedGreenDiamond = "Cards/OneStripedGreenDiamond.png", "One", "Striped", "Green", "Diamond"
+OneStripedGreenRound = "Cards/OneStripedGreenRound.png", "One", "Striped", "Green", "Round"
+OneStripedGreenSquiggle = "Cards/OneStripedGreenSquiggle.png", "One", "Striped", "Green", "Squiggle"
+OneStripedRedDiamond = "Cards/OneStripedRedDiamond.png", "One", "Striped", "Red", "Diamond"
+OneStripedRedRound = "Cards/OneStripedRedRound.png", "One", "Striped", "Red", "Round"
+OneStripedRedSquiggle = "Cards/OneStripedRedSquiggle.png", "One", "Striped", "Red", "Squiggle"
+TwoBlankBlueDiamond  = "Cards/TwoBlankBlueDiamond.png", "Two", "Blank", "Blue", "Diamond"
+TwoBlankBlueRound = "Cards/TwoBlankBlueRound.png", "Two", "Blank", "Blue", "Round"
+TwoBlankBlueSquiggle = "Cards/TwoBlankBlueSquiggle.png", "Two", "Blank", "Blue", "Squiggle"
+TwoBlankGreenDiamond = "Cards/TwoBlankGreenDiamond.png", "Two", "Blank", "Green", "Diamond"
+TwoBlankGreenRound = "Cards/TwoBlankGreenRound.png", "Two", "Blank", "Green", "Round"
+TwoBlankGreenSquiggle = "Cards/TwoBlankGreenSquiggle.png", "Two", "Blank", "Green", "Squiggle"
+TwoBlankRedDiamond = "Cards/TwoBlankRedDiamond.png", "Two", "Blank", "Red", "Diamond"
+TwoBlankRedRound = "Cards/TwoBlankRedRound.png", "Two", "Blank", "Red", "Round"
+TwoBlankRedSquiggle = "Cards/TwoBlankRedSquiggle.png", "Two", "Blank", "Red", "Squiggle"
+TwoFilledBlueDiamond  = "Cards/TwoFilledBlueDiamond.png", "Two", "Filled", "Blue", "Diamond"
+TwoFilledBlueRound = "Cards/TwoFilledBlueRound.png", "Two", "Filled", "Blue", "Round"
+TwoFilledBlueSquiggle = "Cards/TwoFilledBlueSquiggle.png", "Two", "Filled", "Blue", "Squiggle"
+TwoFilledGreenDiamond = "Cards/TwoFilledGreenDiamond.png", "Two", "Filled", "Green", "Diamond"
+TwoFilledGreenRound = "Cards/TwoFilledGreenRound.png", "Two", "Filled", "Green", "Round"
+TwoFilledGreenSquiggle = "Cards/TwoFilledGreenSquiggle.png", "Two", "Filled", "Green", "Squiggle"
+TwoFilledRedDiamond = "Cards/TwoFilledRedDiamond.png", "Two", "Filled", "Red", "Diamond"
+TwoFilledRedRound = "Cards/TwoFilledRedRound.png", "Two", "Filled", "Red", "Round"
+TwoFilledRedSquiggle = "Cards/TwoFilledRedSquiggle.png", "Two", "Filled", "Red", "Squiggle"
+TwoStripedBlueDiamond  = "Cards/TwoStripedBlueDiamond.png", "Two", "Striped", "Blue", "Diamond"
+TwoStripedBlueRound = "Cards/TwoStripedBlueRound.png", "Two", "Striped", "Blue", "Round"
+TwoStripedBlueSquiggle = "Cards/TwoStripedBlueSquiggle.png", "Two", "Striped", "Blue", "Squiggle"
+TwoStripedGreenDiamond = "Cards/TwoStripedGreenDiamond.png", "Two", "Striped", "Green", "Diamond"
+TwoStripedGreenRound = "Cards/TwoStripedGreenRound.png", "Two", "Striped", "Green", "Round"
+TwoStripedGreenSquiggle = "Cards/TwoStripedGreenSquiggle.png", "Two", "Striped", "Green", "Squiggle"
+TwoStripedRedDiamond = "Cards/TwoStripedRedDiamond.png", "Two", "Striped", "Red", "Diamond"
+TwoStripedRedRound = "Cards/TwoStripedRedRound.png", "Two", "Striped", "Red", "Round"
+TwoStripedRedSquiggle = "Cards/TwoStripedRedSquiggle.png", "Two", "Striped", "Red", "Squiggle"
+ThreeBlankBlueDiamond  = "Cards/ThreeBlankBlueDiamond.png", "Three", "Blank", "Blue", "Diamond"
+ThreeBlankBlueRound = "Cards/ThreeBlankBlueRound.png", "Three", "Blank", "Blue", "Round"
+ThreeBlankBlueSquiggle = "Cards/ThreeBlankBlueSquiggle.png", "Three", "Blank", "Blue", "Squiggle"
+ThreeBlankGreenDiamond = "Cards/ThreeBlankGreenDiamond.png", "Three", "Blank", "Green", "Diamond"
+ThreeBlankGreenRound = "Cards/ThreeBlankGreenRound.png", "Three", "Blank", "Green", "Round"
+ThreeBlankGreenSquiggle = "Cards/ThreeBlankGreenSquiggle.png", "Three", "Blank", "Green", "Squiggle"
+ThreeBlankRedDiamond = "Cards/ThreeBlankRedDiamond.png", "Three", "Blank", "Red", "Diamond"
+ThreeBlankRedRound = "Cards/ThreeBlankRedRound.png", "Three", "Blank", "Red", "Round"
+ThreeBlankRedSquiggle = "Cards/ThreeBlankRedSquiggle.png", "Three", "Blank", "Red", "Squiggle"
+ThreeFilledBlueDiamond  = "Cards/ThreeFilledBlueDiamond.png", "Three", "Filled", "Blue", "Diamond"
+ThreeFilledBlueRound = "Cards/ThreeFilledBlueRound.png", "Three", "Filled", "Blue", "Round"
+ThreeFilledBlueSquiggle = "Cards/ThreeFilledBlueSquiggle.png", "Three", "Filled", "Blue", "Squiggle"
+ThreeFilledGreenDiamond = "Cards/ThreeFilledGreenDiamond.png", "Three", "Filled", "Green", "Diamond"
+ThreeFilledGreenRound = "Cards/ThreeFilledGreenRound.png", "Three", "Filled", "Green", "Round"
+ThreeFilledGreenSquiggle = "Cards/ThreeFilledGreenSquiggle.png", "Three", "Filled", "Green", "Squiggle"
+ThreeFilledRedDiamond = "Cards/ThreeFilledRedDiamond.png", "Three", "Filled", "Red", "Diamond"
+ThreeFilledRedRound = "Cards/ThreeFilledRedRound.png", "Three", "Filled", "Red", "Round"
+ThreeFilledRedSquiggle = "Cards/ThreeFilledRedSquiggle.png", "Three", "Filled", "Red", "Squiggle"
+ThreeStripedBlueDiamond  = "Cards/ThreeStripedBlueDiamond.png", "Three", "Striped", "Blue", "Diamond"
+ThreeStripedBlueRound = "Cards/ThreeStripedBlueRound.png", "Three", "Striped", "Blue", "Round"
+ThreeStripedBlueSquiggle = "Cards/ThreeStripedBlueSquiggle.png", "Three", "Striped", "Blue", "Squiggle"
+ThreeStripedGreenDiamond = "Cards/ThreeStripedGreenDiamond.png", "Three", "Striped", "Green", "Diamond"
+ThreeStripedGreenRound = "Cards/ThreeStripedGreenRound.png", "Three", "Striped", "Green", "Round"
+ThreeStripedGreenSquiggle = "Cards/ThreeStripedGreenSquiggle.png", "Three", "Striped", "Green", "Squiggle"
+ThreeStripedRedDiamond = "Cards/ThreeStripedRedDiamond.png", "Three", "Striped", "Red", "Diamond"
+ThreeStripedRedRound = "Cards/ThreeStripedRedRound.png", "Three", "Striped", "Red", "Round"
+ThreeStripedRedSquiggle = "Cards/ThreeStripedRedSquiggle.png", "Three", "Striped", "Red", "Squiggle"
+
+cardsList = [OneBlankBlueDiamond, OneBlankBlueRound, OneBlankBlueSquiggle, OneBlankGreenDiamond,
+             OneBlankGreenRound, OneBlankGreenSquiggle, OneBlankRedDiamond, OneBlankRedRound,
+             OneBlankRedSquiggle, OneFilledBlueDiamond, OneFilledBlueRound, OneFilledBlueSquiggle,
+             OneFilledGreenDiamond, OneFilledGreenRound, OneFilledGreenSquiggle, OneFilledRedDiamond,
+             OneFilledRedRound, OneFilledRedSquiggle, OneStripedBlueDiamond, OneStripedBlueRound,
+             OneStripedBlueSquiggle, OneStripedGreenDiamond, OneStripedGreenRound, OneStripedGreenSquiggle,
+             OneStripedRedDiamond, OneStripedRedRound, OneStripedRedSquiggle, TwoBlankBlueDiamond,
+             TwoBlankBlueRound, TwoBlankBlueSquiggle, TwoBlankGreenDiamond, TwoBlankGreenRound,
+             TwoBlankGreenSquiggle, TwoBlankRedDiamond, TwoBlankRedRound, TwoBlankRedSquiggle,
+             TwoFilledBlueDiamond, TwoFilledBlueRound, TwoFilledBlueSquiggle, TwoFilledGreenDiamond,
+             TwoFilledGreenRound, TwoFilledGreenSquiggle, TwoFilledRedDiamond, TwoFilledRedRound,
+             TwoFilledRedSquiggle, TwoStripedBlueDiamond, TwoStripedBlueRound, TwoStripedBlueSquiggle,
+             TwoStripedGreenDiamond, TwoStripedGreenRound, TwoStripedGreenSquiggle, TwoStripedRedDiamond,
+             TwoStripedRedRound, TwoStripedRedSquiggle, ThreeBlankBlueDiamond, ThreeBlankBlueRound,
+             ThreeBlankBlueSquiggle, ThreeBlankGreenDiamond, ThreeBlankGreenRound, ThreeBlankGreenSquiggle,
+             ThreeBlankRedDiamond, ThreeBlankRedRound, ThreeBlankRedSquiggle, ThreeFilledBlueDiamond,
+             ThreeFilledBlueRound, ThreeFilledBlueSquiggle, ThreeFilledGreenDiamond, ThreeFilledGreenRound,
+             ThreeFilledGreenSquiggle, ThreeFilledRedDiamond, ThreeFilledRedRound, ThreeFilledRedSquiggle,
+             ThreeStripedBlueDiamond, ThreeStripedBlueRound, ThreeStripedBlueSquiggle, ThreeStripedGreenDiamond,
+             ThreeStripedGreenRound, ThreeStripedGreenSquiggle, ThreeStripedRedDiamond, ThreeStripedRedRound,
+             ThreeStripedRedSquiggle]
